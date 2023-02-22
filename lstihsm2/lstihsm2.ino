55fe33dd911c0cb3ecb263e9f7f28a85dc1f29a8
DESKTOP-3NH060D\acer <test@gmail.com>
2023-02-22 08:03:49 +0700

    no message

---
+++ lstihsm2/lstihsm2.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  pinMode(2, OUTPUT);
+//  digitalWrite(2, HIGH);
+//  delay(100);
+//  digitalWrite(2, LOW);
+//  delay(100);
+}
